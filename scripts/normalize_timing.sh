bb8fed357eba87304d2a7ab6002aeea3a4e7675d
Martin Kröning <mkroening@posteo.net>
2020-07-13 14:52:02 +0200

    Split timings

--- scripts/normalize_timing.sh
+++ scripts/normalize_timing.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
+split() {
+    src="$1"
+
+    head -20000 "$src" | sed '2~2d' > "$src"_odd
+    head -20000 "$src" | sed '1~2d' > "$src"_even
+}
+
 normalize() {
     src="$1"
     dest="$1"_normalized
 
-    head -10000 "$src" > "$dest"
-    perl -pe 's/.*Time: (\d+\.\d+ \S+).*/\1/g' "$dest" | sponge "$dest"
+    perl -pe 's/.*Time: (\d+\.\d+ \S+).*/\1/g' "$src" > "$dest"
     sed --in-place -E 's/ μs//g; s/(.+ [^μ]s)/units --terse "\1" μs/e' "$dest"
 
     echo "Normalized $src as $dest"
 }
 
 for timing in $*; do
-    normalize "$timing"
+    split "$timing"
+    normalize "$timing"_odd
+    normalize "$timing"_even
 done
